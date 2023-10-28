a7958896aa50cce630d82b1800205d1dd26d77d0
Estherr18 <138602792+Estherr18@users.noreply.github.com>
2023-10-28 18:38:19 -0400

    Add files via upload

---
+++ Python/Characters of John/index.py
@@ -0,0 +1,7 @@
+
+character_name = "Jesus"
+character_age = "30"
+print("There is One God named " + character_name + ", ")
+print("He started His Ministry before the age of " + character_age + ", ")
+print("He loves being " + character_age + ".")
+print("But He especially is Love for His name is" + character_name + ".")
---
+++ Python/Input/input.py
@@ -0,0 +1,2 @@
+name = input("Enter your name: ")
+password =  input("Enter your password ")
